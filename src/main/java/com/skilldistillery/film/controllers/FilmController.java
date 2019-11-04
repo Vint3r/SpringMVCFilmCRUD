67d0fde55321bb6f36b75d825fb8b9f7db12f1aa
Adam <adamonwan@gmail.com>
2019-11-03 17:00:50 -0700

    commit

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -99,7 +99,7 @@ public class FilmController {
 	public ModelAndView deleteFilm(@Valid Film film, Errors errors) {
 		ModelAndView mv = new ModelAndView();
 		if (!dao.deleteFilm(film)) {
-			errors.rejectValue("id", "error.id", "Unable to delete film from data base");
+			errors.rejectValue("film", "error.film", "Unable to delete film from data base");
 			mv.addObject("film", film);
 			mv.setViewName("WEB-INF/displayfullinfo.jsp");
 			return mv;
